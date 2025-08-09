d4e3c4a84ec5637e8f57f7f180cf41676decc10f
Ben Brobst <brobst464@gmail.com>
2025-08-08 22:23:31 -0400

    finished the program!!!

--- main.cpp
+++ main.cpp
@@ -1,3 +1,27 @@
-int main() {
+#include "Calculator.h"
+#include <iostream>
 
+
+int main() {
+	Calculator c1(22, 85);
+	Calculator c2(55, 0);
+
+	std::cout << "\n";
+	c1.displayValues();
+	std::cout << "\n";
+	std::cout << "Result of adding: " << c1.add() << "\n";
+	std::cout << "Result of subtracting: " << c1.subtract() << "\n";
+	std::cout << "Result of multiplying: " << c1.multiply() << "\n";
+	std::cout << "Result of dividing: " << c1.divide() << "\n";
+
+	std::cout << "\n";
+	std::cout << "\n";
+	std::cout << "\n";
+
+	c2.displayValues();
+	std::cout << "\n";
+	std::cout << "Result of adding: " << c2.add() << "\n";
+	std::cout << "Result of subtracting: " << c2.subtract() << "\n";
+	std::cout << "Result of multiplying: " << c2.multiply() << "\n";
+	std::cout << "Result of dividing: " << c2.divide() << "\n";
 }
